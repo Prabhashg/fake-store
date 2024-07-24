84f1fb87f8703014c43360fe1919b7c8cd341a5a
Prabhash Kumar <prabhash.kumar@indxtechnology.com>
2024-07-24 15:26:08 +0530

    v2

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
-let buildTable = function (data) {
-    data.forEach(element => {
-        let row = `<tr>
-        <td>${element.title}</td>
-        <td>${element.description}</td>
-        <td>$${element.price}</td>
-        <td>${element.rating.rate} (${element.rating.count})</td>
-        <td><img src="${element.image}" alt="" style="width:60px; height:60px"></td>
+async function buildTable() {
+    let selectedNum = document.getElementById('numOfProduct').value
+    console.log(selectedNum);
+
+    try {
+        let data = await fetch('https://fakestoreapi.com/products?limit=100').then(res => {
+            return res.json();
+        });
+
+        document.getElementById('numOfProduct').setAttribute('max', data.length);
+
+        document.getElementById('products').innerHTML = "";
+        for (let i = 0; i < selectedNum; i++) {
+            let row = `<tr>
+        <td>${data[i].id}</td>   
+        <td>${data[i].title}</td>
+        <td>${data[i].description}</td>
+        <td>$${data[i].price}</td>
+        <td>${data[i].rating.rate} <br> (${data[i].rating.count})</td>
+        <td><img src="${data[i].image}" alt="" style="width:60px; height:60px"></td>
         </tr>`
 
-        document.getElementById('products').innerHTML += row;
-    })
-}
+            document.getElementById('products').innerHTML += row;
+        }
 
-fetch('https://fakestoreapi.com/products?limit=100')
-    .then(res => {
-        return res.json();
-    })
-    .then(data => {
-        buildTable(data)
-    })
-    .catch(err => {
-        console.log(err);
+    } catch (error) {
+        console.log(error);
         document.getElementsByClassName('container').innerHTML = "";
         document.getElementById('errorMessage').innerHTML = "Something Unexpected happened";
-    });
+    }
+
+
+
+}
+
+buildTable();
+
+document.getElementById('numOfProductBtn').addEventListener('click', buildTable);
