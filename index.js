59018022d8b2b0e85e4bff44dc7c463b4498ce6c
Prabhash Kumar <prabhash.kumar@indxtechnology.com>
2024-07-25 10:51:56 +0530

    v3

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 async function buildTable() {
-    let selectedNum = document.getElementById('numOfProduct').value
-    console.log(selectedNum);
+    let selectedNum = document.getElementById('numOfProductInputBox').value
 
     try {
         let data = await fetch('https://fakestoreapi.com/products?limit=100').then(res => {
             return res.json();
         });
 
-        document.getElementById('numOfProduct').setAttribute('max', data.length);
+        document.getElementById('numOfProductInputBox').setAttribute('max', data.length);
 
         document.getElementById('products').innerHTML = "";
         for (let i = 0; i < selectedNum; i++) {
             let row = `<tr>
-        <td>${data[i].id}</td>   
-        <td>${data[i].title}</td>
-        <td>${data[i].description}</td>
-        <td>$${data[i].price}</td>
-        <td>${data[i].rating.rate} <br> (${data[i].rating.count})</td>
-        <td><img src="${data[i].image}" alt="" style="width:60px; height:60px"></td>
+        <td style="color: rgb(233, 233, 233); padding: 2%">${data[i].id}</td>   
+        <td style="color: rgba(198,245,248,255); padding-left: 20px;">${data[i].title}</td>
+        <td style="color: rgb(233, 233, 233); padding-left: 10px;">${data[i].description}</td>
+        <td style="color: rgb(233, 233, 233); padding: 60px">$${data[i].price}</td>
+        <td style="color: rgb(233, 233, 233); padding-left: 70px">${data[i].rating.rate} <br> (${data[i].rating.count})</td>
+        <td><img src="${data[i].image}" alt="" style="width:60px; height:60px; padding-left: 30px"></td>
         </tr>`
 
             document.getElementById('products').innerHTML += row;
